a14a888e406e507b09dd907090ba46a5116a69c4
Deny Pătrașcu <denypatrascu@gmail.com>
2021-02-26 10:24:18 +0200

    Update docs mechanism

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,17 +1,30 @@
-import { Config as StencilConfig } from '@stencil/core';
+import { Config as StencilConfig } from "@stencil/core";
+import { generator } from "@webcardinal/internals";
 
-export interface CardinalConfig extends StencilConfig {
+export interface WebCardinalConfig extends StencilConfig {
+  readonly component: string
   readonly useBootstrap: boolean
 }
 
-export const config: CardinalConfig = {
-  namespace: 'webcardinal',
-  globalScript: './src/globals/index.ts',
+export const config: WebCardinalConfig = {
+  component: "@cardinal/wiki",
+  namespace: "webcardinal",
+  globalScript: "./src/globals/index.ts",
   outputTargets: [
     {
-      type: 'dist',
-      dir: 'build/dist'
+      type: "dist",
+      dir: "build/dist"
+    },
+    {
+      type: "docs-readme",
+      dir: "docs/readme",
+      // strict: true,
+      footer: "*Made by [WebCardinal](https://github.com/webcardinal) contributors.*"
+    },
+    {
+      type: "docs-custom",
+      generator
     }
   ],
   useBootstrap: true
-}
+};
